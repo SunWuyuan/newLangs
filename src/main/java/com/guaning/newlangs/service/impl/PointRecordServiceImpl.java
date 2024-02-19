38e28a5c19d222592ab8bfef26bb0ef54a49f877
SunWuyuan <1847261658@qq.com>
2024-02-19 21:33:16 +0800

    1

--- src/main/java/com/guaning/newlangs/service/impl/PointRecordServiceImpl.java
+++ src/main/java/com/guaning/newlangs/service/impl/PointRecordServiceImpl.java
@@ -38,87 +38,169 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 @Service
 public class PointRecordServiceImpl extends ServiceImpl<PointRecordMapper, PointRecord> implements PointRecordService {
-    final ConfigurableListableBeanFactory beanFactory;
-    private final UserService userService;
-
-    @Resource
-    private RedisTemplate<String, Integer> redisTemplate;
-
-    public PointRecordServiceImpl(ConfigurableListableBeanFactory beanFactory, UserService userService) {
-        this.beanFactory = beanFactory;
-        this.userService = userService;
-    }
-
-    public ConfigService getConfigService() {
-        return beanFactory.getBean(ConfigService.class);
-    }
-
-    // Point Record List
-    @Override
-    public SaResult list(int page, int pageSize) {
-        // Create a pagination builder
-        Page<PointRecord> recordPage = new Page<>(page, pageSize);
-
-        if (StpUtil.hasRole("1")) {
-            page(recordPage, Wrappers.<PointRecord>lambdaQuery().orderByDesc(PointRecord::getCreatedTime));
-        } else {
-            page(recordPage, Wrappers.<PointRecord>lambdaQuery().eq(PointRecord::getUserId, StpUtil.getLoginIdAsLong())
-                    .orderByDesc(PointRecord::getCreatedTime));
-        }
-
-        return SaResult.data(recordPage);
-    }
-
-    // Add Point Record
-    @Override
-    public SaResult add(PointRecord dto) {
-        save(dto);
-
-        return SaResult.ok("Point record added successfully");
-    }
-
-    // Modify Points
-    @Override
-    public SaResult edit(PointRecord dto) {
-        Long userId = dto.getUserId();
-        String action = dto.getAction();
-        Integer balance = dto.getBalance();
-        int rest;
-        String remark = dto.getRemark();
-
-        Integer oldPoint = userService.getById(userId).getPoint();
-        if (action.equals("Increase")) {
-            rest = oldPoint + balance;
-        } else if (action.equals("Deduct")) {
-            rest = oldPoint - balance;
-            if (rest < 0) {
-                return SaResult.error("Exceeded the point deduction limit, please re-enter");
-            }
-        } else {
-            return SaResult.error("Invalid Action parameter");
-        }
-
-        PointRecord pointRecord = new PointRecord();
-        pointRecord.setUserId(userId);
-        pointRecord.setAction(action);
-        pointRecord.setBalance(balance);
-        pointRecord.setRest(rest);
-        pointRecord.setRemark(remark);
-        pointRecord.setCreatedTime(LocalDateTime.now());
-        add(pointRecord);
-
-        User user = new User();
-        user.setId(userId);
-        user.setPoint(rest);
-        user.setUpdatedTime(LocalDateTime.now());
-        userService.updateById(user);
-
-        return SaResult.ok("Points modified successfully");
-    }
-
-    // Buy Points
-    public void buyPoint(BuyPointDto dto) {
-    }
-
-
+	final ConfigurableListableBeanFactory beanFactory;
+	private final UserService userService;
+	
+	@Resource
+	private RedisTemplate<String, Integer> redisTemplate;
+	
+	public PointRecordServiceImpl(ConfigurableListableBeanFactory beanFactory, UserService userService) {
+		this.beanFactory = beanFactory;
+		this.userService = userService;
+	}
+	
+	public ConfigService getConfigService() {
+		return beanFactory.getBean(ConfigService.class);
+	}
+	
+	//积分记录列表
+	@Override
+	public SaResult list(int page, int pageSize) {
+		//构造分页构造器
+		Page<PointRecord> recordPage = new Page<>(page, pageSize);
+		
+		if (StpUtil.hasRole("1")) {
+			page(recordPage, Wrappers.<PointRecord>lambdaQuery().orderByDesc(PointRecord::getCreatedTime));
+		} else {
+			page(recordPage, Wrappers.<PointRecord>lambdaQuery().eq(PointRecord::getUserId, StpUtil.getLoginIdAsLong()).orderByDesc(PointRecord::getCreatedTime));
+		}
+		
+		return SaResult.data(recordPage);
+	}
+	
+	//添加积分记录
+	@Override
+	public SaResult add(PointRecord dto) {
+		save(dto);
+		
+		return SaResult.ok("积分记录添加成功");
+	}
+	
+	//修改积分
+	@Override
+	public SaResult edit(PointRecord dto) {
+		Long userId = dto.getUserId();
+		String action = dto.getAction();
+		Integer balance = dto.getBalance();
+		int rest;
+		String remark = dto.getRemark();
+		
+		Integer oldPoint = userService.getById(userId).getPoint();
+		if (action.equals("增加")) {
+			rest = oldPoint + balance;
+		} else if (action.equals("扣除")){
+			rest = oldPoint - balance;
+			if (rest < 0) {
+				return SaResult.error("超出积分扣除上限，请重新填写");
+			}
+		} else {
+			return SaResult.error("Action参数有误");
+		}
+		
+		PointRecord pointRecord = new PointRecord();
+		pointRecord.setUserId(userId);
+		pointRecord.setAction(action);
+		pointRecord.setBalance(balance);
+		pointRecord.setRest(rest);
+		pointRecord.setRemark(remark);
+		pointRecord.setCreatedTime(LocalDateTime.now());
+		save(pointRecord);
+		
+		User user = new User();
+		user.setId(userId);
+		user.setPoint(rest);
+		user.setUpdatedTime(LocalDateTime.now());
+		userService.updateById(user);
+		
+		return SaResult.ok("修改积分成功");
+	}
+	
+	//生成兑换码
+	@Override
+	public SaResult createPointCode(int point) {
+		//生成兑换码
+		String pointCode = GenSerial.createCode();
+		
+		//兑换码5分钟有效期
+		try {
+			//兑换码缓存到redis中，并设置有效期5分钟
+			redisTemplate.opsForValue().set(pointCode, point, 5, TimeUnit.MINUTES);
+		} catch (Exception e) {
+			return SaResult.error("redis缓存失败");
+		}
+		
+		HashMap<String, Object> map = new HashMap<>();
+		map.put("pointCode", pointCode);
+		map.put("langsPoint", point);
+		
+		return SaResult.data(map);
+	}
+	
+	//兑换积分
+	@Override
+	public SaResult exchangePoint(String pointCode) {
+		Long userId = StpUtil.getLoginIdAsLong();
+		int commonPoint = 0;
+		
+		List<Config> configList = getConfigService().list();
+		for (Config con : configList) {
+			if (con.getK().equals("common_point")) {
+				commonPoint = Integer.parseInt(con.getV());
+			}
+		}
+		
+		//通用兑换码
+		if (pointCode.equals("我爱OurWorld(域名服务)")) {
+			PointRecord pointRecord = new PointRecord();
+			User user = userService.getById(userId);
+			int oldPoint = user.getPoint();
+			
+			if (user.getIsExchange() == 1) {
+				return SaResult.error("您已经兑换过了，请等待下一次机会");
+			} else {
+				pointRecord.setUserId(userId);
+				pointRecord.setAction("增加");
+				pointRecord.setBalance(commonPoint);
+				pointRecord.setRest(oldPoint + commonPoint);
+				pointRecord.setRemark("兑换通用积分");
+				pointRecord.setCreatedTime(LocalDateTime.now());
+				add(pointRecord);
+				
+				user.setId(userId);
+				user.setPoint(oldPoint + commonPoint);
+				user.setIsExchange(1);
+				userService.updateById(user);
+				
+				return SaResult.ok("兑换成功");
+			}
+		} else {
+			//购买兑换码
+			//从redis获取兑换码对应的积分
+			Integer point = redisTemplate.opsForValue().get(pointCode);
+			if (point == null) {
+				return SaResult.error("兑换码错误");
+			} else {
+				PointRecord pointRecord = new PointRecord();
+				User user = userService.getById(userId);
+				int oldPoint = user.getPoint();
+				
+				pointRecord.setUserId(userId);
+				pointRecord.setAction("增加");
+				pointRecord.setBalance(point);
+				pointRecord.setRest(oldPoint + point);
+				pointRecord.setRemark("购买积分");
+				pointRecord.setCreatedTime(LocalDateTime.now());
+				add(pointRecord);
+				
+				user.setId(userId);
+				user.setPoint(oldPoint + point);
+				userService.updateById(user);
+				
+				//兑换成功删除redis缓存
+				redisTemplate.delete(pointCode);
+				
+				return SaResult.ok("兑换成功");
+			}
+		}
+	}
 }
--- src/main/java/com/guaning/newlangs/service/impl/UserServiceImpl.java
+++ src/main/java/com/guaning/newlangs/service/impl/UserServiceImpl.java
@@ -114,8 +114,8 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
 
 		try {
 			// 邮件发送验证码
-			MailUtil.send(account, CollUtil.newArrayList(email), "NewLangs",
-					"您的厚浪邮箱验证码为：" + code + ",验证码有效期为2分钟,感谢您的注册,祝您使用愉快！!", false);
+			MailUtil.send(account, CollUtil.newArrayList(email), "OurWorld域名服务验证码",
+					"您的OurWorld域名服务邮箱验证码为：" + code + ",验证码有效期为2分钟,感谢您的注册,祝您使用愉快！!", false);
 		} catch (Exception e) {
 			// 发送失败，删除缓存
 			redisTemplate.delete(email);
@@ -142,8 +142,8 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
 			return SaResult.error("redis缓存失败");
 		}
 		String username = "houlangs";
-		String APIKEY = "";  // 填写你的短信宝apikey
-		String content = "【二级域名】您的验证码为：" + code;
+		String APIKEY = "7ee4e5e22aa7479cbd8cbbe3beb917c0";
+		String content = "【OurWorld(域名服务)域名】您的验证码为：" + code;
 		log.info("code = {}", code);
 
 		String result = SMSAPI.sms(username, APIKEY, phone, content);
@@ -214,7 +214,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
 		}
 
 		String url = "https://eid.shumaidata.com/eid/check";
-		String appCode = ""; // 填写你的appcode
+		String appCode = "0870bf07f5cc42fbb652e9ed207ebb88";
 		Map<String, String> params = new HashMap<>();
 		params.put("idcard", idNumber);
 		params.put("name", name);
@@ -305,7 +305,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
 	public SaResult login(LoginDto dto, HttpServletRequest request) {
 		// 判断是否登录
 		if (StpUtil.isLogin()) {
-			return SaResult.error("已经登录过了");
+			return SaResult.error("已经登录过了，<a href=\"https://app.houlangs.com/\">点此返回</a>");
 		}
 
 		// 查询用户
@@ -401,6 +401,18 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
 		if (user == null) {
 			return SaResult.error("账号不存在");
 		}
+
+		// 更新邮箱
+		// if (!StrUtil.hasBlank(dto.getEmail())) {
+		// if (StpUtil.hasRole("1")) {
+		// user.setEmail(dto.getEmail());
+		// user.setUpdatedTime(LocalDateTime.now());
+		// updateById(user);
+		// } else {
+		// return SaResult.error("需要管理员权限");
+		// }
+		// }
+
 		// 更新密码
 		if (!StrUtil.hasBlank(dto.getPassword())) {
 			// 写入参数
@@ -482,8 +494,8 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
 
 			try {
 				// 邮件发送验证码
-				MailUtil.send(account, CollUtil.newArrayList(email), "二级域名分发", "<p>尊敬的用户：</p>" +
-						"我们很抱歉地通知您，由于您违反了使用协议，我们封禁了您的账户，并删除了账户内的全部解析。"
+				MailUtil.send(account, CollUtil.newArrayList(email), "OurWorld(域名服务)域名", "<p>尊敬的用户：</p>" +
+						"我们很抱歉地通知您，由于您违反了<a href=\"https://forum.houlangs.com/d/2-hou-lang-zheng-ce-lie-biao\">OurWorld(域名服务)相关使用政策</a>，我们封禁了您的账户，并删除了账户内的全部解析。"
 						+
 						" 为给您带来不便表示歉意，我们深感抱歉。若你认为封禁您的账号有待商榷，请回复此邮件。我们将尽最大努力保障您的权益，并在尽可能短的时间内解决这一问题。感谢您的耐心等待和理解。",
 						false);
@@ -585,6 +597,6 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
 		} else {
 			return SaResult.error("已经签到过了，请明天再来");
 		}
-		return SaResult.ok("签到成功");
+		return SaResult.ok("签到成功，获得1积分");
 	}
 }
